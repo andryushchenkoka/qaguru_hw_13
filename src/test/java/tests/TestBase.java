8ff99c2acf10e3350a3c1c7d2c2565975f210eb3
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-17 18:56:56 +0300

    Добавлен Allure в конфигурацию BaseTest

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -10,5 +12,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.headless = true;
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 }
