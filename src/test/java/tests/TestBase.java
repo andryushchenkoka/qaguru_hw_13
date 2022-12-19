74a41fd057a7a738b78315e95c51f16dcb6f3094
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-19 16:52:09 +0300

    Добавлен класс Attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,15 +2,22 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aspectj.weaver.bcel.AtAjAttributes;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestBase {
 
     @BeforeAll
     public static void beforeSettings() {
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
@@ -19,6 +26,19 @@ public class TestBase {
 
     @BeforeEach
     public void beforeEach() {
+
         SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    public void addAttach() {
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+        Date date = new Date();
+
+        Attach.screenshotAs("screenshot_" + dateFormat.format(date));
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
