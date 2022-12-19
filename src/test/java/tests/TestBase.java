9e22607bc6b37aec948611622e79ed650c107e29
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-19 15:41:09 +0300

    Добавлены таймаут и  ссылка на Selenoid

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,7 +3,9 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
@@ -11,7 +13,12 @@ public class TestBase {
     public static void beforeSettings() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; // ссылка на selenoid
+    }
 
+    @BeforeEach
+    public void beforeEach() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 }
